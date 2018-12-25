6179928257ed6194267d367f2ed9804c7739b783
Joy917 <296351685@qq.com>
2018-12-25 21:44:42 +0800

    修改相关注解，测通数据库读写

--- src/main/java/com/bangbang/baseframe/service/DemoService.java
+++ src/main/java/com/bangbang/baseframe/service/DemoService.java
@@ -1,8 +1,5 @@
 package com.bangbang.baseframe.service;
 
-import javax.transaction.Transactional;
-
-@Transactional
 public interface DemoService {
     String saveDemo(String name, Integer age);
 }
